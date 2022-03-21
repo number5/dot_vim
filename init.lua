93b8dfd36353a43ccc86ed214050ce1f65defe2c
Bruce Wang <number5@users.noreply.github.com>
2022-03-22 09:31:55 +1100

    step 1

--- init.lua
+++ init.lua
@@ -1,13 +1,23 @@
--- TODO
--- * Setup DAP (debugger)
-
+-- debug only
 vim.o.runtimepath = "/Users/bruce.wang/dotfiles/nvim/," .. vim.o.runtimepath
 
 print(vim.o.runtimepath)
 
 vim.g.mapleader = " "
 
-require("configs.general")
-require("plugins")
-require("configs.theme")
-require("configs.remaps")
+local modules = {
+   "core.options",
+   "plugins",
+   "core.mappings",
+}
+
+for _, module in ipairs(modules) do
+   local ok, err = pcall(require, module)
+   if not ok then
+      error("Error loading " .. module .. "\n\n" .. err)
+   end
+end
+
+-- non plugin mappings
+-- require("core.mappings").misc()
+
