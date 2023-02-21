bf8396ec9d7b166f58ef9a7f5218d1a201b7c85f
Bruce Wang <number5@users.noreply.github.com>
2023-02-22 00:18:01 +1100

    almost there

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,14 +3,7 @@ return {
     "rebelot/heirline.nvim",
     event = "VeryLazy",
     config = function()
-      local heirline = require("heirline")
-      local statuslines = require("ui.heirline.statuslines")
-
-      local opts = {
-        statusline = statuslines.StatusLines,
-      }
-
-      heirline.setup(opts)
+      require("ui.heirline.init").load()
     end,
   },
 }
