3cdf6edf83e00301f8a5530436c9644324577bdf
Bruce Wang <number5@users.noreply.github.com>
2023-03-04 00:46:18 +1100

    ts-rainbow2

---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,3 @@
+return {
+  { url = "https://gitlab.com/HiPhish/nvim-ts-rainbow2.git" },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = {
+      rainbow = {
+        enable = true,
+        -- list of languages you want to disable the plugin for
+        disable = { "jsx", "cpp" },
+        -- Which query to use for finding delimiters
+        query = "rainbow-parens",
+        strategy = require "ts-rainbow.strategy.local",
+      },
+    },
+  },
+}
