8ffa1f55b15ab04d098aae5b38e1376f14adf6fc
Bruce Wang <number5@users.noreply.github.com>
2023-03-23 15:58:13 +1100

    updates

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,183 @@
+-- INFO: Server names are LSP names, not Mason names
+-- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
+local lsp_servers = {
+  "lua_ls",
+  "yamlls",
+  "jsonls",
+  "cssls",
+  "emmet_ls", -- css & html completion
+  "pyright", -- python
+  "marksman", -- markdown
+  "tsserver", -- ts/js
+  "eslint", -- ts/js
+  "bashls", -- also used for zsh
+  "taplo", -- toml
+}
+
+--------------------------------------------------------------------------------
+
+local lspSettings = {}
+local lspFileTypes = {}
+
+-- https://github.com/LuaLS/lua-language-server/wiki/Annotations#annotations
+-- https://github.com/LuaLS/lua-language-server/wiki/Settings
+lspSettings.lua_ls = {
+  Lua = {
+    format = { enable = false }, -- using stylua instead. Also, sumneko-lsp-formatting has this weird bug where all folds are opened
+    completion = {
+      callSnippet = "Replace",
+      keywordSnippet = "Replace",
+      displayContext = 2,
+      postfix = ".",
+    },
+    -- libraries defined per-project via luarc.json location: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = { "vim" },
+      disable = { "trailing-space" },
+    },
+    hint = {
+      enable = true,
+      setType = true,
+      paramName = "All",
+      paramType = true,
+      arrayIndex = "Disable",
+    },
+    telemetry = { enable = false },
+  },
+}
+
+-- https://github.com/sublimelsp/LSP-css/blob/master/LSP-css.sublime-settings
+lspSettings.cssls = {
+  css = {
+    lint = {
+      vendorPrefix = "ignore",
+      propertyIgnoredDueToDisplay = "error",
+      universalSelector = "ignore",
+      float = "ignore",
+      boxModel = "ignore",
+      -- since these would be duplication with stylelint
+      duplicateProperties = "ignore",
+      emptyRules = "warning",
+    },
+    colorDecorators = { enable = true }, -- not supported yet
+  },
+}
+
+-- https://github.com/typescript-language-server/typescript-language-server#workspacedidchangeconfiguration
+local jsAndTsSettings = {
+  inlayHints = {
+    includeInlayEnumMemberValueHints = true,
+    includeInlayFunctionLikeReturnTypeHints = true,
+    includeInlayFunctionParameterTypeHints = true,
+    includeInlayParameterNameHints = "all", -- none | literals | all
+    includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+    includeInlayPropertyDeclarationTypeHints = true,
+    includeInlayVariableTypeHints = true,
+    includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+  },
+}
+
+lspSettings.tsserver = {
+  completions = { completeFunctionCalls = true },
+  typescript = jsAndTsSettings,
+  javascript = jsAndTsSettings,
+  -- https://github.com/microsoft/TypeScript/blob/master/src/compiler/diagnosticMessages.json
+  diagnostics = { ignoredCode = {} },
+}
+
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint
+lspSettings.eslint = {
+  quiet = false, -- = include warnings
+  codeAction = {
+    disableRuleComment = { location = "sameLine" }, -- add ignore-comments on the same line
+  },
+}
+
+-- https://github.com/sublimelsp/LSP-json/blob/master/LSP-json.sublime-settings
+lspSettings.jsonls = {
+  json = {
+    validate = { enable = true },
+    format = { enable = true },
+  },
+}
+
+-- https://github.com/redhat-developer/yaml-language-server#language-server-settings
+lspSettings.yamlls = {
+  yaml = { keyOrdering = false }, -- FIX mapKeyOrder
+}
+
+--------------------------------------------------------------------------------
+
+lspFileTypes.bashls = { "sh", "zsh", "bash" } -- force lsp to work with zsh
+lspFileTypes.emmet_ls = { "css", "scss", "html" }
+
+--------------------------------------------------------------------------------
+
+-- Enable snippet capability for completion (nvim_cmp)
+local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
+lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- Enable folding (nvim-ufo)
+lspCapabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
+}
+
+--------------------------------------------------------------------------------
+
+return {
+  { -- package manager
+    "williamboman/mason.nvim",
+    lazy = true,
+    config = function()
+      require("mason").setup {
+        ui = {
+          border = BorderStyle,
+          icons = {
+            package_installed = "✓",
+            package_pending = "羽",
+            package_uninstalled = "✗",
+          },
+        },
+      }
+    end,
+  },
+  { -- auto-install lsp servers
+    "williamboman/mason-lspconfig.nvim",
+    event = "VeryLazy",
+    dependencies = "williamboman/mason.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = lsp_servers,
+      }
+    end,
+  },
+
+  { -- configure LSPs
+    "neovim/nvim-lspconfig",
+    dependencies = "folke/neodev.nvim", -- lsp for nvim-lua config
+    init = function()
+      -- INFO plugin must be setup before lua lsp-config setup
+      require("neodev").setup {
+        library = { plugins = false },
+      }
+
+      -- LSP Server setup
+      for _, lsp in pairs(lsp_servers) do
+        local config = {
+          capabilities = lspCapabilities,
+          settings = lspSettings[lsp], -- if no settings, will assign nil and therefore to nothing
+          filetypes = lspFileTypes[lsp],
+        }
+        require("lspconfig")[lsp].setup(config)
+      end
+
+      -- Border Styling
+      require("lspconfig.ui.windows").default_options.border = BorderStyle
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = BorderStyle })
+      vim.lsp.handlers["textDocument/signatureHelp"] =
+        vim.lsp.with(vim.lsp.handlers.signature_help, { border = BorderStyle })
+    end,
+  },
+}
