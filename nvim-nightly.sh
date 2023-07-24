88a66d68c6b90808bb6a6cf93f3892b775da8c2a
Bruce Wang <number5@users.noreply.github.com>
2023-07-25 01:08:57 +1000

    use gh

--- nvim-nightly.sh
+++ nvim-nightly.sh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
-cd /tmp
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+#wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+gh release download -R neovim/neovim nightly -p "*macos.tar.gz" -D /tmp
 cd
 rm -rf ~/nvim-macos
 tar xzvf /tmp/nvim-macos.tar.gz
