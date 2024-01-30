8bb4c9c70d1c033a44f876152653e62d40d917dd
Bruce Wang <number5@users.noreply.github.com>
2024-01-31 03:47:40 +1100

    rename folder

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
-vim.g.linterConfigFolder = vim.fn.stdpath "config" .. "linterConfigs"
+vim.g.linterConfigFolder = vim.fn.stdpath "config" .. "/linterConfigs"
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require "config.lazy"
