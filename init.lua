f6b2db17dadb17e08ef1405463dc6cc532839809
Bruce Wang <number5@users.noreply.github.com>
2024-01-31 03:44:17 +1100

    lspconfig

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
+vim.g.linterConfigFolder = vim.fn.stdpath "config" .. "linterConfigs"
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require "config.lazy"
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,22 +1,112 @@
-local lsp_servers = {
-  "lua-language-server",
-  "yaml-language-server",
-  "json-lsp",
-  "python-lsp-server", -- python
-  "marksman", -- markdown
-  "eslint_d", -- ts/js
-  "bash-language-server", -- also used for zsh
-  "taplo", -- toml
+---since nvim-lspconfig and mason.nvim use different package names
+---mappings from https://github.com/williamboman/mason-lspconfig.nvim/blob/main/lua/mason-lspconfig/mappings/server.lua
+---@type table<string, string>
+local lspToMasonMap = {
+  bashls = "bash-language-server",
+  biome = "biome", -- ts/js/json linter/formatter
+  efm = "efm", -- linter integration, only used for shellcheck in zsh files
+  emmet_language_server = "emmet-language-server", -- css/html completions
+  jedi_language_server = "jedi-language-server", -- python lsp (with better hovers)
+  pylsp = "python-lsp-server",
+  jsonls = "json-lsp",
+  lua_ls = "lua-language-server",
+  marksman = "marksman", -- markdown lsp
+  taplo = "taplo", -- toml lsp
+  typos_lsp = "typos-lsp", -- spellchecker for code
+  vale_ls = "vale-ls", -- natural language linter
+  yamlls = "yaml-language-server",
 }
 
 --------------------------------------------------------------------------------
 
-local lspSettings = {}
-local lspFileTypes = {}
+---@class (exact) lspConfiguration see https://github.com/neovim/nvim-lspconfig/blob/master/doc/lspconfig.txt#L46
+---@field autostart? boolean
+---@field capabilities? table <string, string|table|boolean|function>
+---@field cmd? string[]
+---@field filetypes? string[]
+---@field handlers? table <string, function>
+---@field init_options? table <string, string|table|boolean>
+---@field on_attach? function(client, bufnr)
+---@field on_new_config? function(new_config, root_dir)
+---@field root_dir? function(filename, bufnr)
+---@field settings? table <string, table>
+---@field single_file_support? boolean
+
+---@type table<string, lspConfiguration>
+local serverConfigs = {}
+for lspName, _ in pairs(lspToMasonMap) do
+  serverConfigs[lspName] = {}
+end
+
+---Creates code-action keymap on attaching lsp to buffer
+---@param mode string|string[]
+---@param lhs string
+---@param codeActionTitle string used by `string.find`
+---@param keymapDesc string
+local function codeActionKeymap(mode, lhs, codeActionTitle, keymapDesc)
+  vim.keymap.set(mode, lhs, function()
+    vim.lsp.buf.code_action {
+      filter = function(action)
+        return action.title:find(codeActionTitle)
+      end,
+      apply = true,
+    }
+  end, { buffer = true, desc = keymapDesc })
+end
+
+--------------------------------------------------------------------------------
+-- BASH / ZSH
+
+-- DOCS https://github.com/bash-lsp/bash-language-server/blob/main/server/src/config.ts
+
+-- PENDING https://github.com/bash-lsp/bash-language-server/issues/1064
+-- disable shellcheck via LSP to avoid double-diagnostics
+serverConfigs.bashls = {
+  settings = {
+    bashIde = { shellcheckPath = "" },
+  },
+}
+
+-- HACK use efm to use shellcheck with zsh files
+-- EFM: Markdown & Shell
+serverConfigs.efm = {
+  cmd = { "efm-langserver", "-c", vim.g.linterConfigFolder .. "/efm.yaml" },
+  filetypes = { "sh", "markdown" }, -- limit to filestypes needed
+}
+
+local efmDependencies = {
+  "shellcheck", -- PENDING https://github.com/bash-lsp/bash-language-server/issues/663
+  "markdownlint",
+}
+--------------------------------------------------------------------------------
+-- DOCS https://luals.github.io/wiki/settings/
+serverConfigs.lua_ls = {
+  settings = {
+    Lua = {
+      completion = {
+        callSnippet = "Replace",
+        keywordSnippet = "Replace",
+        showWord = "Disable", -- don't suggest common words as fallback
+        postfix = ".", -- useful for `table.insert` and the like
+      },
+      diagnostics = {
+        globals = { "vim" }, -- when contributing to nvim plugins missing a `.luarc.json`
+        disable = { "trailing-space" }, -- formatter already does that
+      },
+      hint = { -- inlay hints
+        enable = true,
+        setType = true,
+        arrayIndex = "Disable",
+      },
+      workspace = { checkThirdParty = "Disable" }, -- FIX https://github.com/sumneko/lua-language-server/issues/679#issuecomment-925524834
+      telemetry = { enable = false },
+    },
+  },
+}
 
 -- Not using ruff-lsp, use python-lsp-ruff instead
 -- lspSettings.ruff_lsp = {}
-lspSettings["python-lsp-server"] = {
+serverConfigs.pylsp = {
   plugins = {
     ruff = {
       enabled = true,
@@ -24,213 +114,114 @@ lspSettings["python-lsp-server"] = {
     },
   },
 }
--- https://github.com/LuaLS/lua-language-server/wiki/Annotations#annotations
--- https://github.com/LuaLS/lua-language-server/wiki/Settings
-lspSettings["lua-language-server"] = {
-  Lua = {
-    format = { enable = false }, -- using stylua instead. Also, sumneko-lsp-formatting has this weird bug where all folds are opened
-    workspace = { checkThirdParty = false },
-    completion = {
-      callSnippet = "Replace",
-      keywordSnippet = "Replace",
-      displayContext = 2,
-      postfix = ".",
-    },
-    -- libraries defined per-project via luarc.json location: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = { "vim" },
-      disable = { "trailing-space" },
-    },
-    hint = {
-      enable = true,
-      setType = true,
-      paramName = "All",
-      paramType = true,
-      arrayIndex = "Disable",
-    },
-    telemetry = { enable = false },
+
+-- DOCS https://github.com/pappasam/jedi-language-server#configuration
+serverConfigs.jedi_language_server = {
+  init_options = {
+    diagnostics = { enable = true },
+    codeAction = { nameExtractVariable = "extracted_var", nameExtractFunction = "extracted_func" },
+  },
+  -- HACK since init_options cannot be changed during runtime, we need to use
+  -- `on_new_config` to set it.
+  on_new_config = function(config, root_dir)
+    -- Since `vim.env.VIRTUAL_ENV` is not set in time, we need to hardcode the
+    -- identification of the venv-dir here
+    local venv_python = root_dir .. "/.venv/bin/python"
+    local fileExists = vim.loop.fs_stat(venv_python) ~= nil
+    if not fileExists then
+      return
+    end
+    config.init_options.workspace = { environmentPath = venv_python }
+  end,
+  on_attach = function()
+		-- use jedi instead of refactoring.nvim
+		-- stylua: ignore start
+		codeActionKeymap({ "n", "x" }, "<leader>fe", "^Extract expression into variable", " Extract Var")
+		codeActionKeymap({ "n", "x" }, "<leader>fE", "^Extract expression into function", " Extract Func")
+    -- stylua: ignore end
+  end,
+}
+
+-- DOCS https://github.com/Microsoft/vscode/tree/main/extensions/json-language-features/server#configuration
+-- Disable formatting in favor of biome
+serverConfigs.jsonls = {
+  init_options = {
+    provideFormatter = false,
+    documentRangeFormattingProvider = false,
   },
 }
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint
-lspSettings.eslint_d = {
-  quiet = false, -- = include warnings
-  codeAction = {
-    disableRuleComment = { location = "sameLine" }, -- add ignore-comments on the same line
+-- SIC needs to be enabled, can be removed with nvim 0.10 support for dynamic config
+serverConfigs.biome = {
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = true
+    client.server_capabilities.documentRangeFormattingProvider = true
+  end,
+}
+
+-- TYPOS
+-- DOCS https://github.com/tekumara/typos-vscode#settings
+serverConfigs.typos_lsp = {
+  init_options = { diagnosticSeverity = "information" },
+}
+
+-- VALE
+-- DOCS https://vale.sh/docs/integrations/guide/#vale-ls
+-- DOCS https://vale.sh/docs/topics/config#search-process
+serverConfigs.vale_ls = {
+  init_options = {
+    configPath = vim.g.linterConfigFolder .. "/vale/vale.ini",
+    installVale = true, -- needs to be set, since false by default
+    syncOnStartup = false,
   },
+  -- just needs any root directory to work, we are providing the config already
+  root_dir = function()
+    return os.getenv "HOME"
+  end,
 }
 
--- https://github.com/sublimelsp/LSP-json/blob/master/LSP-json.sublime-settings
-lspSettings["json-lsp"] = {
-  json = {
-    validate = { enable = true },
-    format = { enable = true },
-  },
-}
+-- FIX https://github.com/errata-ai/vale-ls/issues/4
+vim.env.VALE_CONFIG_PATH = vim.g.linterConfigFolder .. "/vale/vale.ini"
 
 --------------------------------------------------------------------------------
 
-lspFileTypes.bashls = { "sh", "zsh", "bash" } -- force lsp to work with zsh
-
---------------------------------------------------------------------------------
-
--- Enable snippet capability for completion (nvim_cmp)
-local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
-lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
-
--- Enable folding (nvim-ufo)
-lspCapabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
-
---------------------------------------------------------------------------------
-
-return {
-  { -- package manager
-    "williamboman/mason.nvim",
-    lazy = true,
-    dependencies = {
-      -- Additional lua configuration. Setup defore setting up lspconfig for lua_ls.
-      "folke/neodev.nvim",
-
-      -- Hover guide for function signatures.
-      "ray-x/lsp_signature.nvim",
-    },
-    opts = {
-      ensure_installed = lsp_servers,
-      diagnostics = {
-        float = {
-          source = "always",
-        },
-        virtual_text = false,
+-- DOCS https://github.com/redhat-developer/yaml-language-server/tree/main#language-server-settings
+serverConfigs.yamlls = {
+  settings = {
+    yaml = {
+      format = {
+        enable = true,
+        printWidth = 120,
+        proseWrap = "always",
       },
     },
   },
-
-  { -- configure LSPs
-    "neovim/nvim-lspconfig",
-    dependencies = "folke/neodev.nvim", -- lsp for nvim-lua config
-    init = function()
-      -- INFO plugin must be setup before lua lsp-config setup
-      require("neodev").setup {
-        library = { plugins = false },
-      }
-
-      -- LSP Server setup
-      -- for _, lsp in pairs(lsp_servers) do
-      --   local config = {
-      --     capabilities = lspCapabilities,
-      --     settings = lspSettings[lsp], -- if no settings, will assign nil and therefore to nothing
-      --     filetypes = lspFileTypes[lsp],
-      --   }
-      --   require("lspconfig")[lsp].setup(config)
-      -- end
-
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = BorderStyle })
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-        vim.lsp.handlers.signature_help,
-        { border = BorderStyle }
-      )
-    end,
-    config = function(_, opts)
-      local on_attach = function(client, bufnr)
-        -- Attach the server to Navbuddy only if server is not ruff_lsp.
-        -- ruff_lsp does not support documentSymbols.
-        if client.name ~= "ruff_lsp" then
-          require("nvim-navbuddy").attach(client, bufnr)
-        end
-
-        local nmap = function(keys, func, desc)
-          if desc then
-            desc = "LSP: " .. desc
-          end
-
-          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-        end
-
-        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-        nmap("<C-c>", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-        nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-        nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-        nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-        -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-        -- See `:help K` for why this keymap
-        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-        vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Help" })
-        -- nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-        -- Lesser used LSP functionality
-        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-        -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-        -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-        -- nmap('<leader>wl', function()
-        --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        -- end, '[W]orkspace [L]ist Folders')
-
-        -- Create a command `:Format` local to the LSP buffer
-        vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-          vim.lsp.buf.format()
-        end, { desc = "Format current buffer with LSP" })
-        nmap("<leader>bf", "<cmd>Format<CR>", "[B]uffer [F]ormat")
-      end
-
-      -- Add a border to the hover frame.
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-
-      require("neodev").setup()
-
-      vim.diagnostic.config(opts.diagnostics)
-
-      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-      -- Ensure the servers above are installed
-      local mason_lspconfig = require "mason-lspconfig"
-
-      mason_lspconfig.setup {
-        ensure_installed = vim.tbl_keys(opts.servers),
-      }
-
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = opts.servers[server_name],
-          }
-        end,
-      }
-
-      -- Moved out of mason_lspconfig handlers loop because diagnostics
-      -- were not being disabled.
-      require("lspconfig").jedi_language_server.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        init_options = {
-          diagnostics = { enable = false },
-        },
-      }
-
-      -- Nixd
-      require("lspconfig").nixd.setup {}
-
-      -- Workaround for warning when using clang-format (via null-ls) with clangd lsp.
-      capabilities.offsetEncoding = { "utf-16" }
-
-      require("lspconfig").clangd.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        init_options = {
-          diagnostics = { enable = false },
-        },
-      }
-    end,
+  -- SIC needs enabling via setting *and* via capabilities to work. Probably
+  -- fixed with nvim 0.10 supporting dynamic config changes
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = true
+  end,
+}
+--------------------------------------------------------------------------------
+return {
+  "neovim/nvim-lspconfig",
+  lazy = false,
+  mason_dependencies = vim.list_extend(efmDependencies, vim.tbl_values(lspToMasonMap)),
+  dependencies = { -- loading as dependency ensures it's loaded before lua_ls
+    "folke/neodev.nvim",
+    opts = { library = { plugins = false } }, -- too slow with all my plugins
   },
+  config = function()
+    require("lspconfig.ui.windows").default_options.border = vim.g.borderStyle
+
+    -- Enable snippets-completion (nvim_cmp) and folding (nvim-ufo)
+    local lspCapabilities = vim.lsp.protocol.make_client_capabilities()
+    lspCapabilities.textDocument.completion.completionItem.snippetSupport = true
+    lspCapabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
+
+    for lsp, serverConfig in pairs(serverConfigs) do
+      serverConfig.capabilities = lspCapabilities
+      require("lspconfig")[lsp].setup(serverConfig)
+    end
+  end,
 }
