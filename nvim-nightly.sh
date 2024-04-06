2fd45f3f014c2c4cde2ee50e11197663768bb683
Bruce Wang <number5@users.noreply.github.com>
2024-04-07 02:52:40 +1000

    updates

--- nvim-nightly.sh
+++ nvim-nightly.sh
@@ -1,8 +1,9 @@
 #! /bin/zsh
 
-#wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
-gh release download -R neovim/neovim nightly -p "*macos.tar.gz" -D /tmp
+#wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
+MAC_ARM64_BINARY="nvim-macos-arm64.tar.gz"
+gh release download -R neovim/neovim nightly -p "${MAC_ARM64_BINARY}" -D /tmp
 cd
 rm -rf ~/nvim-macos
-tar xzvf /tmp/nvim-macos.tar.gz
-ln -sf ~/nvim-macos/bin/nvim ~/bin/nvim
+tar xzvf /tmp/${MAC_ARM64_BINARY}
+ln -sf ~/nvim-macos-arm64/bin/nvim ~/bin/nvim
