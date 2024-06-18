57fc26189ee4ea9cb1cf90cd9970dd671a6875e1
Bruce Wang <number5@users.noreply.github.com>
2024-06-18 14:04:36 +1000

    wip

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable expandtab for Makefile
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "make" },
+  callback = function()
+    vim.bo[0].expandtab = false -- Use tabs for indentation
+  end,
+})
