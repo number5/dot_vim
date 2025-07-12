3555f979d9ba4b49495d9dbcdffd81a5ab066ba6
Bruce Wang <number5@users.noreply.github.com>
2025-07-13 02:03:45 +1000

    updates

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,4 +1,4 @@
 return {
   { "dmmulroy/ts-error-translator.nvim" },
-  { "pmizio/typescript-tools.nvim", dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" }, opts = {} },
+  -- { "pmizio/typescript-tools.nvim", dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" }, opts = {} },
 }
